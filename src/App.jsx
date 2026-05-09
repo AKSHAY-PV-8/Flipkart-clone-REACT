ceda9108ce884ac4b19991541bbee8600ad40350
AKSHAY-PV-8 <akshaypv19@gmail.com>
2026-05-10 00:10:10 +0530

    refactor

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import {BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Home from "./pages/Home"
 import ProductListPage from "./pages/ProductListPage"
 import ProductDetails from "./pages/ProductDetails"
-import AddtoCartPage from "./pages/AddtoCartPage"
+
 
 function App() {
   return (
@@ -12,7 +12,6 @@ function App() {
           {/* <Route path="/" element={<Home></Home>}/> */}
           <Route path="/" element={<ProductListPage></ProductListPage>}/>
           <Route path="/productDetail/:id" element={<ProductDetails></ProductDetails>}/>
-          <Route path= "/cart/:id" element={<AddtoCartPage></AddtoCartPage>}/>
         </Routes>
       </Router>
   )
